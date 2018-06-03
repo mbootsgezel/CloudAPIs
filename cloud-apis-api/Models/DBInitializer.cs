7111556073c0d7995e66834b909e477be0337a5c
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-06-04 00:44:09 +0200

    Added ifEmpty for Games & Publishers

--- cloud-apis-api/Models/DBInitializer.cs
+++ cloud-apis-api/Models/DBInitializer.cs
@@ -1,7 +1,54 @@
-namespace cloud_apis_api {
-    public class DBInitializer {
-        public static void Initialize(GamesContext context) {
+namespace cloud_apis_api
+{
+    public class DBInitializer
+    {
+        public static void Initialize(GamesContext context)
+        {
             context.Database.EnsureCreated();
+
+            /* 
+                context.Games.Any() werkt niet ?
+             */
+            // if (!context.Games.Any())
+            // {
+
+            //     var pb1 = new Publisher()
+            //     {
+            //         PublisherName = "Epic Games"
+            //     };
+            //     var pb2 = new Publisher()
+            //     {
+            //         PublisherName = "Bungie"
+            //     };
+            //     var pb3 = new Publisher()
+            //     {
+            //         PublisherName = "Valve"
+            //     };
+
+            //     var g1 = new Game() {
+            //         Title = "Gears of War",
+            //         Publisher = pb1
+            //     };
+            //     var g2 = new Game() {
+            //         Title = "Halo",
+            //         Publisher = pb2
+            //     };
+            //     var g3 = new Game() {
+            //         Title = "Half-Life",
+            //         Publisher = pb3
+            //     };
+
+            //     context.Publishers.Add(pb1);
+            //     context.Publishers.Add(pb2);
+            //     context.Publishers.Add(pb3);
+
+            //     context.Games.Add(g1);
+            //     context.Games.Add(g2);
+            //     context.Games.Add(g3);
+
+            //     context.SaveChanges();
+            // }
+
         }
     }
 }
